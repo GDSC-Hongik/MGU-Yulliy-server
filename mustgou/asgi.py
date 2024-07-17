01dd11dd9e1949cda61710e6fa877ada41d7367a
권아림 <arim@gwon-alim-ui-noteubug.local>
2024-07-18 01:16:41 +0900

    chore: commit lint 세팅

--- mustgou/asgi.py
+++ mustgou/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mustgou.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mustgou.settings")
 
 application = get_asgi_application()
