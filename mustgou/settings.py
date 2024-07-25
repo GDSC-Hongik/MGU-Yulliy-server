d84824915894c07d253b7dd5de1ac2ec3606d852
권아림 <arim@gwon-alim-ui-noteubug.local>
2024-07-25 16:19:49 +0900

    chore: .env 추가

--- mustgou/settings.py
+++ mustgou/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path, os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-p@#&)c8s!q2!b2mfn)=39fqd@g&rz&zhy@r(p5-xu40cc-)r=%"
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
