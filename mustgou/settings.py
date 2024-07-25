3c252059887de2a1399b7b1cd60e5276626aa388
권아림 <arim@gwon-alim-ui-noteubug.local>
2024-07-26 04:11:18 +0900

    chore: MySQL DB 연결

--- mustgou/settings.py
+++ mustgou/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = "mustgou.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "MuGoU",
+        "USER": "root",
+        "PASSWORD": config("DB_PASSWORD"),
+        "HOST": "127.0.0.1",
+        "PORT": "3306",
     }
 }
 
