b9f9a23e19b924b8c3f2bdacd8cb80deb7431543
권아림 <arim@gwon-alim-ui-noteubug.local>
2024-07-26 23:26:10 +0900

    chore: model 작성

--- friends/models.py
+++ friends/models.py
@@ -1,3 +1,10 @@
-# from django.db import models
+from django.db import models
+from users.models import User
 
 # Create your models here.
+
+
+class Friend(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="friends")
+    friend = models.ForeignKey(User, on_delete=models.CASCADE, related_name="friend_of")
+    state = models.CharField(max_length=20, null=True, blank=True)
