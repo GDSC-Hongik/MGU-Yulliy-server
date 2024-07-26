4c2a6cc73af9b2f041ce2ebed783c7622a6fc4b8
권아림 <arim@gwon-alim-ui-noteubug.local>
2024-07-26 23:26:10 +0900

    chore: model 작성

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,3 +1,19 @@
-# from django.db import models
+from django.db import models
 
 # Create your models here.
+
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=255)
+    rating_naver = models.DecimalField(
+        max_digits=5, decimal_places=2, null=True, blank=True
+    )
+    rating_kakao = models.DecimalField(
+        max_digits=5, decimal_places=2, null=True, blank=True
+    )
+    rating_google = models.DecimalField(
+        max_digits=5, decimal_places=2, null=True, blank=True
+    )
+    address = models.CharField(max_length=255)
+    latitude = models.DecimalField(max_digits=10, decimal_places=7)
+    longitude = models.DecimalField(max_digits=10, decimal_places=7)
--- users/models.py
+++ users/models.py
@@ -1,3 +1,11 @@
-# from django.db import models
+from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    email = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    nickname = models.CharField(max_length=255, null=True, blank=True)
+    image = models.CharField(max_length=255, null=True, blank=True)
+    reliability = models.SmallIntegerField(null=True, blank=True)
