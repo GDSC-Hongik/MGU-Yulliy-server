0ffcdbf66c756732c9554a02948d2fc69101091c
권아림 <arim@gwon-alim-ui-noteubug.local>
2024-07-26 22:50:17 +0900

    chore: app 생성

--- mustgou/settings.py
+++ mustgou/settings.py
@@ -38,7 +38,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "myapp",
+    "users",
+    "friends",
+    "restaurants",
+    "reviews",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
--- reviews/apps.py
+++ reviews/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class MyappConfig(AppConfig):
+class ReviewsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "myapp"
+    name = "reviews"
