bad8f3dbcd1b2ced4d20a6cca93f211420287829
dkfla <arim329@naver.com>
2024-07-30 19:46:46 +0900

    chore: latitude model 수정

--- restaurants/models.py
+++ restaurants/models.py
@@ -15,5 +15,5 @@ class Restaurant(models.Model):
         max_digits=5, decimal_places=2, null=True, blank=True
     )
     address = models.CharField(max_length=255)
-    latitude = models.DecimalField(max_digits=10, decimal_places=7)
-    longitude = models.DecimalField(max_digits=10, decimal_places=7)
+    latitude = models.DecimalField(max_digits=11, decimal_places=8)
+    longitude = models.DecimalField(max_digits=11, decimal_places=8)
