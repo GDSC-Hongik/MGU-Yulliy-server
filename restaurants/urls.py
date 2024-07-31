f48870676475c8b5ddba52fa3a2a031c23ce4c92
Shim Kyumin <cyan52863@gmail.com>
2024-07-31 12:47:15 +0900

    feat: 내 맛집 리스트 기능 구현

--- restaurants/urls.py
+++ restaurants/urls.py
@@ -3,7 +3,13 @@ from . import views
 
 
 urlpatterns = [
-    path("restaurants/", views.restaurant_list, name="restaurant-list"),
+    #    path("restaurants/", views.restaurant_list, name="restaurant-list"),
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
     path("search/", views.search, name="search"),
+    path(
+        "restaurants/<int:pk>/",
+        views.add_remove_restaurant,
+        name="add-remove-restaurant",
+    ),
+    path("restaurants/", views.user_restaurant_list, name="user-restaurant-list"),
 ]
