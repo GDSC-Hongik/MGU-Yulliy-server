8fc639d118ce55a1abf47f5e8c95e0f93dfb1b94
dkfla <arim329@naver.com>
2024-08-01 15:02:27 +0900

    chore: users 앱 삭제

--- friends/migrations/0001_initial.py
+++ friends/migrations/0001_initial.py
@@ -8,7 +8,7 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ("users", "0001_initial"),
+        ("accounts", "0001_initial"),
     ]
 
     operations = [
