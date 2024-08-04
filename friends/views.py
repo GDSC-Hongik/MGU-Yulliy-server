8d1d883ed46975ed545937e1e743008176b8a882
ybkang1108 <ybkang1108@naver.com>
2024-08-05 02:12:29 +0900

    feat: 친구 요청/수락/거절 구현

--- friends/views.py
+++ friends/views.py
@@ -4,6 +4,10 @@ from rest_framework.views import APIView
 
 # from rest_framework.authentication import TokenAuthentication
 # from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+
+# from rest_framework.authentication import TokenAuthentication
+# from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
 
@@ -14,7 +18,7 @@ from .serializers import (
     FriendRequestSerializer,
     RestaurantlistSerializer,
     FriendRecommendSerializer,
-    RestaurantSerializer,
+    # RestaurantSerializer,
 )
 
 # from .serializers import FriendSerializer, FriendRequestSerializer
