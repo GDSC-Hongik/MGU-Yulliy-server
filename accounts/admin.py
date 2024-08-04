b3b9443884e4836db6d6592cadc57b8e1f923598
ybkang1108 <ybkang1108@naver.com>
2024-08-05 02:07:02 +0900

    chore: 어드민 페이지 정리

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,4 +1,18 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import User
 
-admin.site.register(User)
+
+class CustomUserAdmin(UserAdmin):
+    list_display = (
+        "id",
+        "name",
+        "email",
+        "profile_img",
+        "reliability",
+        "date_joined",
+        "last_login",
+    )
+
+
+admin.site.register(User, CustomUserAdmin)
