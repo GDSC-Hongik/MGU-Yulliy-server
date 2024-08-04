35227e9cb2aa610ab2e5bc5565ceb828df049feb
ybkang1108 <ybkang1108@naver.com>
2024-08-05 03:02:32 +0900

    chore: 불필요한 주석 제거

--- friends/models.py
+++ friends/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from accounts.models import User
 
-# Create your models here.
-
 
 class Friend(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="friends")
