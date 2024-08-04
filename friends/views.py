541dd7c42aa839d9c2e8bed8695372abca57ed18
ybkang1108 <ybkang1108@naver.com>
2024-08-05 02:25:15 +0900

    fix: 충돌 수정

--- friends/views.py
+++ friends/views.py
@@ -14,6 +14,7 @@ from .serializers import (
     FriendRequestSerializer,
     RestaurantlistSerializer,
     FriendRecommendSerializer,
+    RestaurantSerializer,
 )
 
 # from .serializers import FriendSerializer, FriendRequestSerializer
