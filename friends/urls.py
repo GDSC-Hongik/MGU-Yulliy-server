a51d529f4728f8c4456b856ee1cb7456f5480105
ybkang1108 <ybkang1108@naver.com>
2024-08-05 02:12:29 +0900

    feat: 친구 요청/수락/거절 구현

--- friends/urls.py
+++ friends/urls.py
@@ -11,8 +11,4 @@ urlpatterns = [
     path("friend-request/", views.FriendRequestView.as_view(), name="friend-request"),
     path("friends/", views.friend_list, name="friend-list"),
     path("friend-recommend/", views.friend_recommend, name="friend-recommend"),
-    # 추가 기능을 테스트하기 위한 url으로 실제로는 /friend 안에서 모두 진행됨
-    path("friend-request/", views.FriendRequestView.as_view(), name="friend-request"),
-    path("friends/", views.friend_list, name="friend-list"),
-    path("friend-recommend/", views.friend_recommend, name="friend-recommend"),
 ]
