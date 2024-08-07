d8427c39fd2c82ca410793e6acf45633443cf22b
ybkang1108 <ybkang1108@naver.com>
2024-08-08 02:13:03 +0900

    feat: 친구 삭제 기능 추가

--- friends/urls.py
+++ friends/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     ),
     # 추가 기능을 테스트하기 위한 url으로 실제로는 /friend 안에서 모두 진행됨
     path("friend-request/", views.FriendRequestView.as_view(), name="friend-request"),
+    path("friends/<int:friend_id>", views.delete_friend, name="delete-friend"),
     path("friends/", views.friend_list, name="friend-list"),
     path("friend-recommend/", views.friend_recommend, name="friend-recommend"),
 ]
