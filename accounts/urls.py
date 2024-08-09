051bebd1e2a045ce18421e2179aeb28f4fce186a
ybkang1108 <ybkang1108@naver.com>
2024-08-09 13:59:41 +0900

    feat: jwt 변경

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,18 @@
-from django.urls import path
-from .views import RegisterView, LoginView
+from django.urls import path, include
+from .views import RegisterView, LoginView, LogoutView, UserViewSet, DeleteUserView
+
+# 뷰셋
+from rest_framework import routers
+from rest_framework_simplejwt.views import TokenRefreshView
+
+router = routers.DefaultRouter()
+router.register("list", UserViewSet)
 
 urlpatterns = [
     path("register/", RegisterView.as_view()),
     path("login/", LoginView.as_view()),
+    path("logout/", LogoutView.as_view()),
+    path("test/", include(router.urls)),
+    path("delete/", DeleteUserView.as_view()),
+    path("refresh/", TokenRefreshView.as_view()),  # jwt 토큰 재발급
 ]
