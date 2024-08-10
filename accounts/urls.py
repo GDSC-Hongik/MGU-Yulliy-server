bc745989898234dda1fbd250b8d859f4813d1caf
ybkang1108 <ybkang1108@naver.com>
2024-08-10 16:59:07 +0900

    fix: 필요 없는 url 제거

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,25 +1,17 @@
-from django.urls import path, include
+from django.urls import path
 from .views import (
     RegisterView,
     LoginView,
     LogoutView,
-    UserViewSet,
     DeleteUserView,
     profile,
 )
-
-# 뷰셋
-from rest_framework import routers
 from rest_framework_simplejwt.views import TokenRefreshView
-
-router = routers.DefaultRouter()
-router.register("list", UserViewSet)
 
 urlpatterns = [
     path("register/", RegisterView.as_view()),
     path("login/", LoginView.as_view()),
     path("logout/", LogoutView.as_view()),
-    path("test/", include(router.urls)),
     path("delete-user/", DeleteUserView.as_view()),
     path("refresh/", TokenRefreshView.as_view()),  # jwt 토큰 재발급
     path("profile/", profile, name="profile"),
