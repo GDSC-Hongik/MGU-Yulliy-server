b04c2e2bfd12b9509f8fd7729054d19d258a5734
dkfla <arim329@naver.com>
2024-08-13 16:00:39 +0900

    fix: profile 테스트

--- accounts/views.py
+++ accounts/views.py
@@ -8,7 +8,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from django.contrib.auth.hashers import check_password
 from rest_framework.permissions import AllowAny
-from rest_framework.decorators import permission_classes, authentication_classes
+from rest_framework.decorators import authentication_classes, permission_classes
 
 from friends.models import Friend, FriendRequest
 from .models import User
@@ -136,6 +136,7 @@ class DeleteUserView(APIView):
 
 
 @api_view(["GET", "PATCH"])
+@authentication_classes([])
 @permission_classes([AllowAny])
 def profile(request):
     user = request.user
