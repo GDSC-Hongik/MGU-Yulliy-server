bf182e70b34cac34e5400a3e792c34bcd75145a3
ybkang1108 <ybkang1108@naver.com>
2024-08-14 22:39:34 +0900

    fix: 토큰 유효시간 설정 변경

--- mustgou/settings.py
+++ mustgou/settings.py
@@ -118,9 +118,9 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 REST_FRAMEWORK = {
-    "DEFAULT_AUTHENTICATION_CLASSES": [
-        "rest_framework_simplejwt.authentication.JWTAuthentication",
-    ],
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_jwt.authentication.JSONWebTokenAuthentication",
+    ),
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
 }
 
